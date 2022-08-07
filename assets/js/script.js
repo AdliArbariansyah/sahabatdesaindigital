7950e4f98351085ba8cf3dc3b24fd7b86fa89ca4
AdliArbariansyah <adliarbariansyah20@gmail.com>
2022-08-07 14:24:38 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function scrollLayanan() {
+  var konten = document.getElementById('layanan');
+  konten.scrollIntoView();
+}
+
+function scrollHome() {
+  var konten = document.getElementById('home');
+  konten.scrollIntoView();
+}
+
+function scrollProfil() {
+  var konten = document.getElementById('profil');
+  konten.scrollIntoView();
+}
+
+function scrollKontak() {
+  var konten = document.getElementById('kontak');
+  konten.scrollIntoView();
+}
+
+function scrollPortfolio() {
+  var konten = document.getElementById('portfolio');
+  konten.scrollIntoView();
+}
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function(){
+//     var currentScrollPos = window.pageYOffset;
+//     if(prevScrollpos > currentScrollPos){
+//         document.getElementById
+//     }
+// }
